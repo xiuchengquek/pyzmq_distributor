02b4c617e022f376ec064efcf475b14de138a499
xiuchengquek <x.quek@garvan.org.au>
2016-10-10 19:22:50 +1100

    Trimmed configuration

--- sinker.py
+++ sinker.py
@@ -1,23 +1,6 @@
 import zmq
-import sys
-import yaml
-import os
 
-
-
-config_file =  os.path.join(os.path.dirname(__file__) , 'config.yaml')
-
-
-with open(config_file) as f:
-    config = yaml.load(f)
-
-sinker_reciever_ip =  config['sinker_listen']
-
-
-
-
-
-def main(receiver_ip):
+if __name__ == '__main__':
     context = zmq.Context()
     # Get reciever
     receiver = context.socket(zmq.PULL)
@@ -25,6 +8,3 @@ def main(receiver_ip):
     while True:
         msg = receiver.recv_unicode()
         print(msg)
-
-if __name__ == '__main__':
-    main(sinker_reciever_ip)
