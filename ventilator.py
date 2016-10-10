9a97677896c917c71b3bb3c9de416b0454df6b38
xiuchengquek <x.quek@garvan.org.au>
2016-10-10 19:23:38 +1100

    Trimmed configuration

--- ventilator.py
+++ ventilator.py
@@ -1,24 +1,10 @@
-import os
-
-
-
 import zmq
 import sys
 
-import yaml
-
-
-
-
-
 if __name__ == '__main__':
-
-
     context = zmq.Context()
     sender = context.socket(zmq.PUSH)
     sender.bind('tcp://*:5555')
-   
-  
     with open(sys.argv[1]) as f:
         for line in f:
             sender.send_unicode(u'%s' % line)
